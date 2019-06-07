3b67c953a1b85392480e991ac56555055f8e3819
samgyeopsal <000321simbs@gmail.com>
2019-06-07 13:50:25 +0900

    백준 나누기

---
+++ Operator_Div/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] arg){
+		Scanner scan = new Scanner(System.in);
+		double a = scan.nextInt();
+		double b = scan.nextInt();
+		if(0<a && b<10){
+		System.out.println((a/b));
+		}
+	}
+}
